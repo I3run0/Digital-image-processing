19a76475c33fad003aad95d05417f19c5c5709f5
Bruno Franca <brunosfranca15@gmail.com>
2024-04-19 18:59:58 -0300

    Updating src files

--- lab02/src/fft_compress.py
+++ lab02/src/fft_compress.py
@@ -1,40 +1,45 @@
 import sys
+import os
 import numpy as np
 import cv2 as cv
 import fft_utils as uts
 
 
 def main(argv):
+    # Check if there are enough command-line arguments
     if len(argv) < 3:
-        print("Usage message: to do")
-        sys.exit()
+        print("Usage: python script.py <compression_level> <input_image_path> <output_image_path>")
+        sys.exit(1)
 
-    cmps_level = float(argv[0])
-    if cmps_level < 1:
-        print("Usafge message: to do")
+    # Extract command-line arguments
+    compression_level = float(argv[0])
+    input_image_path = argv[1]
+    output_image_path = argv[2]
 
-    img = cv.imread(argv[1], cv.IMREAD_GRAYSCALE)
-    out_p = argv[2]
+    # Ensure compression level is valid
+    if compression_level > 1:
+        print("Compression level should be less than or equal to 1.")
+        sys.exit(1)
+
+    # Read the input image in grayscale
+    input_image = cv.imread(input_image_path, cv.IMREAD_GRAYSCALE)
+
+    # Apply FFT to the input image
+    fft = uts.get_fft_from_img(input_image)
+
+    # Determine the threshold value based on compression level
+    threshold_index = round((1 - compression_level) * (fft.size - 1))
+    sorted_fft = np.sort(np.abs(fft).flatten())[::-1]
+    threshold = sorted_fft[threshold_index]
+
+    # Thresholding: set FFT coefficients below the threshold to zero
+    compressed_fft = np.where(np.abs(fft) >= threshold, fft, 0)
+
+    # Reconstruct the image from the modified FFT
+    output_image = uts.get_img_from_fft(compressed_fft)
     
-    fft = uts.get_fft_from_img(img)
-
-    sl_fft = np.reshape(fft, (img.shape[0] * img.shape[1]))
-    sl_fft.sort()
-    sl_fft = sl_fft[::-1]
-    sl_fft = sl_fft[:round(cmps_level * sl_fft.shape[0])]
-    threshold = sl_fft[-1]
-
-    fft[fft < threshold] = 0
-    
-    img_back = uts.get_img_from_fft(fft)
-    
-    cv.imwrite(out_p, img_back)
-
-
-
-
-
-
+    # Write the processed image to the output file
+    cv.imwrite(output_image_path, output_image)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- lab02/src/fft_filters.py
+++ lab02/src/fft_filters.py
@@ -1,17 +1,17 @@
-import sys, os
+import sys
+import os
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 import fft_utils as uttf
 from getopt import getopt
 
-def get_circle_mask(r, cords, shape, fill):
+def get_circle_mask(radius, center_coords, shape, fill):
     """
     Generate a circular mask.
 
     Args:
-        r (int): Radius of the circle.
-        cords (tuple): Coordinates of the circle's center.
+        radius (int): Radius of the circle.
+        center_coords (tuple): Coordinates of the circle's center.
         shape (tuple): Shape of the image.
         fill (bool): Whether to fill the circle or not.
 
@@ -19,73 +19,74 @@ def get_circle_mask(r, cords, shape, fill):
         numpy.ndarray: Binary mask representing the circle.
     """
     mask = np.zeros(shape, dtype='uint8') if fill else np.ones(shape, dtype='uint8')
-    mask = cv.circle(mask, cords, r, 1 if fill else 0, -1)
+    mask = cv.circle(mask, center_coords, radius, 1 if fill else 0, -1)
     return mask
 
-def lower_pass_filter(fft, r):
+def apply_lower_pass_filter(fft, radius):
     """
     Apply a lower pass filter on the Fourier Transform.
 
     Args:
         fft (numpy.ndarray): Input Fourier Transform.
-        r (int): Radius for the filter.
+        radius (int): Radius for the filter.
 
     Returns:
         numpy.ndarray: Filtered Fourier Transform.
     """
-    mask = get_circle_mask(r, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, True)
-    f_fft = fft * mask
-    return f_fft
+    mask = get_circle_mask(radius, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, True)
+    filtered_fft = fft * mask
+    return filtered_fft
 
-def higher_pass_filter(fft, r):
+def apply_higher_pass_filter(fft, radius):
     """
     Apply a higher pass filter on the Fourier Transform.
 
     Args:
         fft (numpy.ndarray): Input Fourier Transform.
-        r (int): Radius for the filter.
+        radius (int): Radius for the filter.
 
     Returns:
         numpy.ndarray: Filtered Fourier Transform.
     """
-    mask = get_circle_mask(r, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, False)
-    f_fft = fft * mask
-    return f_fft
+    mask = get_circle_mask(radius, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, False)
+    filtered_fft = fft * mask
+    return filtered_fft
 
-def band_pass_filter(fft, r_higher, r_lower):
+def apply_band_pass_filter(fft, radius_higher, radius_lower):
     """
     Apply a band pass filter on the Fourier Transform.
 
     Args:
         fft (numpy.ndarray): Input Fourier Transform.
-        r_higher (int): Radius for the higher pass filter.
-        r_lower (int): Radius for the lower pass filter.
+        radius_higher (int): Radius for the higher pass filter.
+        radius_lower (int): Radius for the lower pass filter.
 
     Returns:
         numpy.ndarray: Filtered Fourier Transform.
     """
-    f_fft = higher_pass_filter(fft, r_higher)
-    f_fft = lower_pass_filter(f_fft, r_lower)
-    return f_fft
+    filtered_fft = apply_higher_pass_filter(fft, radius_higher)
+    filtered_fft = apply_lower_pass_filter(filtered_fft, radius_lower)
+    return filtered_fft
 
-def reject_band_filter(fft, r_higher, r_lower):
+def apply_reject_band_filter(fft, radius_higher, radius_lower):
     """
     Apply a reject band filter on the Fourier Transform.
 
     Args:
         fft (numpy.ndarray): Input Fourier Transform.
-        r_higher (int): Radius for the higher pass filter.
-        r_lower (int): Radius for the lower pass filter.
+        radius_higher (int): Radius for the higher pass filter.
+        radius_lower (int): Radius for the lower pass filter.
 
     Returns:
         numpy.ndarray: Filtered Fourier Transform.
     """
-    f_fft = higher_pass_filter(fft, r_higher)
-    mask = get_circle_mask(r_lower, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, True)
-    f_fft[mask == 1] = fft[mask == 1]
-    return f_fft
+    filtered_fft = apply_higher_pass_filter(fft, radius_higher)
+    mask = get_circle_mask(radius_lower, (fft.shape[0] // 2, fft.shape[1] // 2), fft.shape, True)
+    filtered_fft[mask == 1] = fft[mask == 1]
+    return filtered_fft
 
 def main(argv):
+    # Parse command line options
     opts, args = getopt(argv, "m:p:s", ["method=", "parameters=", "spec"])
     method = None
     parameters = None
@@ -94,6 +95,7 @@ def main(argv):
     out_dir = "output_images"
     img_ext = "png"
 
+    # Process command line options
     for opt, arg in opts:
         if opt in ("-m", "--method"):
             method = arg
@@ -102,55 +104,54 @@ def main(argv):
         elif opt in ("-s", "--spec"):
             gen_spec = True
     
+    # Check if method and image path are provided
     if method is None or len(args) < 1:
         print("Method and image path are required.")
         print("Usage: python script.py -m <method> -p <parameters> <image_path>")
         sys.exit(2)
 
+    # Extract image path
     img_path = args[0]
     img_name = os.path.basename(img_path).split(".")[0]
-    if not(os.path.isdir(out_dir)):
-        os.mkdir(out_dir)
-
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
 
-    f_fft, f_mag = None, None
-    
+    # Apply FFT to the image
     fft = uttf.get_fft_from_img(img)
     mag = uttf.get_mag_from_fft(fft)
-    if method == 'low':
-        f_fft = lower_pass_filter(fft, int(parameters[0]))
-        f_mag = lower_pass_filter(mag, int(parameters[0]))
-    elif method == 'high':
-        f_fft = higher_pass_filter(fft, int(parameters[0]))
-        f_mag = higher_pass_filter(mag, int(parameters[0]))
-    elif method == 'band':
-        f_fft = band_pass_filter(fft, int(parameters[0]), int(parameters[1]))
-        f_mag = band_pass_filter(mag, int(parameters[0]), int(parameters[1]))
-    elif method == 'reject':
-        f_fft = reject_band_filter(fft, int(parameters[0]), int(parameters[1]))
-        f_mag = reject_band_filter(mag, int(parameters[0]), int(parameters[1]))
-    
-    img_back = uttf.get_img_from_fft(f_fft)
 
-    '''
-    plt.subplot(121)s
-    plt.imshow(img_back, cmap='gray')
-    plt.title('FFT applied filter')
-    plt.xticks([]), plt.yticks([])
-    plt.subplot(122)
-    plt.imshow(f_mag, cmap='gray')
-    plt.title('Magnitude spectrum')
-    plt.xticks([]), plt.yticks([])
-    plt.show() 
-    '''
+    # Choose filtering method and apply filter
+    if method == 'low':
+        filtered_fft = apply_lower_pass_filter(fft, int(parameters[0]))
+        filtered_mag = apply_lower_pass_filter(mag, int(parameters[0]))
+    elif method == 'high':
+        filtered_fft = apply_higher_pass_filter(fft, int(parameters[0]))
+        filtered_mag = apply_higher_pass_filter(mag, int(parameters[0]))
+    elif method == 'band':
+        filtered_fft = apply_band_pass_filter(fft, int(parameters[0]), int(parameters[1]))
+        filtered_mag = apply_band_pass_filter(mag, int(parameters[0]), int(parameters[1]))
+    elif method == 'reject':
+        filtered_fft = apply_reject_band_filter(fft, int(parameters[0]), int(parameters[1]))
+        filtered_mag = apply_reject_band_filter(mag, int(parameters[0]), int(parameters[1]))
     
-    out_path = f'{out_dir}/{img_name}_{method}_{"_".join(parameters)}'
+    # Reconstruct image from filtered FFT
+    img_back = uttf.get_img_from_fft(filtered_fft)
+   
+    # Create output directory if it doesn't exist
+    if not(os.path.isdir(out_dir)):
+        os.mkdir(out_dir)
+    
+    # Create method-specific subdirectory
+    if not(os.path.isdir(f'{out_dir}/{method}')):
+        os.mkdir(f'{out_dir}/{method}')
+
+    # Save filtered image
+    out_path = f'{out_dir}/{method}/{img_name}_{method}_{"_".join(parameters)}'
     cv.imwrite(f'{out_path}.{img_ext}', img_back)
 
+    # Optionally, generate and save magnitude spectrum images
     if gen_spec:
-        cv.imwrite(f'{out_path}_spec.{img_ext}', mag)
-        cv.imwrite(f'{out_path}_spec_cropped.{img_ext}', f_mag)
-
+        cv.imwrite(f'{out_path}_spec_cropped.{img_ext}', filtered_mag)
+        
+    
 if __name__ == "__main__":
     main(sys.argv[1:])
